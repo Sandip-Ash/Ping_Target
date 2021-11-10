c766b08d28a2b7c8732107a376ea9121783603db
Sandip-Ash <79464800+Sandip-Ash@users.noreply.github.com>
2021-11-10 00:46:43 -0500

    Update ping_IP_2.py

--- ping_IP_2.py
+++ ping_IP_2.py
@@ -1,11 +1,13 @@
 #Program to PING a target system using normal python commands
 #It can run in any python IDLE
-import os
+import os 
+import pyfiglet  
 
-ping = input("Number of pings= ") #Stores the number of ping values
+print(pyfiglet.figlet_format("Auto Ping"))          #for decoration purpose
 
-IP = input("IP address= ") #Stores the IP address
+ping = input("Number of pings= ")                   #Stores the number of ping values
+
+IP = input("IP address= ")                          #Stores the IP address
 
 
-#os.system('ping -c number_of_pings Target_IP')
-os.system('ping -c '+ping+' '+IP)
+os.system('ping -c '+ping+' '+IP)                   #os.system('ping -c number_of_pings Target_IP')
